2a6abc916efcd295513241baa68fea9eae2869e1
Jijo G <145054528+jijog-jijog@users.noreply.github.com>
2025-06-14 10:50:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal('.home-content p, .about-content ',{ origin:'right'});
 /*==================== typed js ====================*/
 
 const typed = new Typed('.multiple-text',{
-    strings:['Frontend Developer','Web Designer','Explorer',],
+    strings:['Software Developer','Java Full-Stack Developer','Web Designer',],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
